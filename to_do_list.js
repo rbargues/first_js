14f6eadb2fc4a84c1c0cb45f63c36c203f927ac5
unknown <rbargues@gmail.com>
2019-11-13 20:47:31 -0500

    i def broke this

--- to_do_list.js
+++ to_do_list.js
@@ -6,31 +6,34 @@ class to_do_list {
 		this.tasks.push(task);
 	}
 	remove_task(task){
-		this.tasks = this.tasks.filter(function(item){return item != task})
+		this.tasks = this.tasks.filter(function(item){return item != task});
+		render_list()
+	}
+	render_list(){
+		var container = document.getElementById("container");
+		while (container.hasChildNodes()){
+			container.removeChild(container.lastChild);
+		};
+		this.tasks.forEach(function(item){
+			var task_item = document.createElement("button");
+				task_item.innerHTML	= "done";
+				task_item.onclick = this.remove_task(item);
+			var task_label = document.createElement("div");
+					task_label.innerHTML = item;
+			container.appendChild(task_label);
+			container.appendChild(task_item);
+			container.appendChild(document.createElement("br"));
+		});
+		 
 	}
 }
 
 const make_list = (list) => {
-	var task_to_add = document.getElementById("item")
-	list.push(task_to_add)
-	var to_do = new to_do_list(list)
+	var task_to_add = document.getElementById("item");
+	list.push(task_to_add);
+	var to_do = new to_do_list(list);
+	to_do.render_list()
+
 }
 
-
-function render_list(arr){
-var container = document.getElementById("container");
-while (container.hasChildNodes()) {
-          container.removeChild(container.lastChild);};
-	to_do_list.forEach(function(item) {
-	var task_item = document.createElement("button");
-		task_item.innerHTML = 'done';
-		task_item.onclick = function(){remove_task(item)};
-	var task_label = document.createElement('div');
-		task_label.innerHTML = item;
-
-	container.appendChild(task_label);
-	container.appendChild(task_item);
-	container.appendChild(document.createElement('br'));
-});
-};
-
+document.getElementById("button").onclick = make_list(list);
